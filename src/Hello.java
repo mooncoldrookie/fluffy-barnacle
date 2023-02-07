ee389c872f334ba09d43ee6e0516233f525da7e4
sunmoon <bettercode365@outlook.com>
2023-02-07 19:43:09 +0800

    Revert "第4次提交"
    This reverts commit 41207608

--- src/Hello.java
+++ src/Hello.java
@@ -8,5 +8,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
