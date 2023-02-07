c71a6300feecf472a0be9e58b5b12de0446bf55c
sunmoon <bettercode365@outlook.com>
2023-02-07 19:38:10 +0800

    第1次提交

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName: Hello
+ * Package: PACKAGE_NAME
+ * description:
+ *
+ * @Author: WangMing
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
